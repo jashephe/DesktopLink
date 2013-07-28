3770dce45a9bcdf3a639e48a1dd10737813279d5
James Shepherdson <james.shepherdson@gmail.com>
2013-07-28 08:59:33 -0400

    Initial Commit

---
+++ DesktopLink/DLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DLAppDelegate.h
+//  DesktopLink
+//
+//  Created by James Shepherdson on 7/28/13.
+//  Copyright (c) 2013 James Shepherdson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DLAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ DesktopLink/DLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  DLAppDelegate.m
+//  DesktopLink
+//
+//  Created by James Shepherdson on 7/28/13.
+//  Copyright (c) 2013 James Shepherdson. All rights reserved.
+//
+
+#import "DLAppDelegate.h"
+
+@implementation DLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
